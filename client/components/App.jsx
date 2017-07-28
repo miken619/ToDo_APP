465a276b86cd11e3f6429079b7cc98d9117c8d7e
Hack Reactor Student <student@hackreactor.com>
2017-07-27 19:53:55 -0700

    Add event handler for input and submission

---
+++ client/components/App.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import ToDoList from './ToDoList.jsx';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lists: [],
+      input: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  componentDidMount() {
+    axios.get('/api/entries/fetchEntry')
+         .then(({data}) => {
+           this.setState({ lists: data });})
+         .catch(error => {
+           console.log('Error in CDM ', error);
+         });
+         
+  }
+
+  handleInputChange(event) {
+    event.preventDefault();
+    this.setState({input: event.target.value}, () => { 
+      console.log('This is the state of input ', this.state.input);
+    });
+  }  
+  
+  handleSubmit(event) {
+    this.setState({lists: [...this.state.lists, this.state.input]});
+    axios.post('/api/entries/createEntry', {
+      errand: this.state.input  
+    }).then(res => {
+      console.log('Here is the response: ', res)
+    }).catch(err => {
+      console.log('Error in teh axios post request for entry ', err);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        TO DO LIST
+        <Form>
+          <input onChange={this.handleInputChange}/>
+        </Form>
+        <Button bsStyle="success" onClick{this.handleSubmit}> Submit</Button>
+        <ToDoList lists={this.state.lists}/>
+      </div>
+    )
+  }
+
+}
