96a8aff2755bbedae30b72a20a6b8c56ab6c9e26
Hack Reactor Student <student@hackreactor.com>
2017-07-28 12:08:18 -0700

    Create connection to postgres db

--- server/db/config.js
+++ server/db/config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-module.exports const db = new Sequelize(,{
+module.exports const db = new Sequelize('postgres://dzthhgqs:ZtspT5KBYPRKn_7MWc1hxgAWUPod9Qfj@babar.elephantsql.com:5432/dzthhgqs',{
   dialect: 'postgres'
 });
 
